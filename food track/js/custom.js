60e87721d0e5bde27732e0d9fa716a34cffcc741
kcj8686 <kcj8686@gmail.com>
2022-10-05 16:32:00 +0900

    portfolio
    ver1_3

---
+++ food track/js/custom.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+// aos 플러그인
+  "use strict";
+
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: false,
+    mirror: false
+  });
+});
+
+
+
+
+
+
+});
